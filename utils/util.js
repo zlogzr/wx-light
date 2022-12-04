3f0807ec89e4911095e4e1266935c67aeec0decb
zhangyang <2224143549@qq.com>
2022-12-04 11:57:28 +0800

    fix: 加入图片cdn

--- utils/util.js
+++ utils/util.js
@@ -1,24 +1,24 @@
-function convertToCastString(casts){
-  var castsjoin = "";
-  for (var idx in casts) {
-    castsjoin = castsjoin + casts[idx].name + " / ";
-  }
-  return castsjoin.substring(0, castsjoin.length - 2);
+function convertToCastString(casts) {
+    var castsjoin = "";
+    for (var idx in casts) {
+        castsjoin = castsjoin + casts[idx].name + " / ";
+    }
+    return castsjoin.substring(0, castsjoin.length - 2);
 }
 
 function convertToCastInfos(casts) {
-  var castsArray = []
-  for (var idx in casts) {
-    var cast = {
-      img: casts[idx].avatars ? casts[idx].avatars.large : "",
-      name: casts[idx].name
+    var castsArray = []
+    for (var idx in casts) {
+        var cast = {
+            img: casts[idx].avatars ? casts[idx].avatars.large : "",
+            name: casts[idx].name
+        }
+        castsArray.push(cast);
     }
-    castsArray.push(cast);
-  }
-  return castsArray;
+    return castsArray;
 }
 
 export {
-  convertToCastString,
-  convertToCastInfos
+    convertToCastString,
+    convertToCastInfos
 }
